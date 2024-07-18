d7591a1672e1607bf12a40bbee12392df6f5e3e7
sketu <SKetU-l@proton.me>
2024-07-18 05:33:15 +0000

    Revert "sdm660-common: Flash dummy super image if needed"
    This reverts commit 87a5cf45eda3e7a2508871cd47c20d493964a176.

--- releasetools.py
+++ releasetools.py
@@ -18,24 +18,6 @@ import hashlib
 import common
 import re
 
-def FullOTA_InstallBegin(info):
-  AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
-  flash_script = open("device/asus/sdm660-common/partitions/flash_super_dummy.sh", 'r').read()
-  common.ZipWriteStr(info.output_zip, "install/bin/flash_super_dummy.sh", flash_script);
-  info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
-  info.script.AppendExtra('run_program("/sbin/sh", "/tmp/flash_super_dummy.sh");')
-  return
-
-def AddImage(info, dir, basename, dest):
-  path = dir + "/" + basename
-  if path not in info.input_zip.namelist():
-    return
-
-  data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
-
 def FullOTA_Assertions(info):
   AddModemAssertion(info)
   return
